7011f598d6aa51a804f686c3af67dfd8c81304a0
dan <danilo.naid@gmail.com>
2016-08-09 22:27:06 +0200

    mysql session 97% done, missing data for user!

--- Trinity Database Editors/Trinity Database Editors/Form3.cs
+++ Trinity Database Editors/Trinity Database Editors/Form3.cs
@@ -6,10 +6,12 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 
 namespace Trinity_Database_Editors
 {
+    
     public partial class Form3 : Form
     {
         public Form3()
@@ -25,6 +27,29 @@ namespace Trinity_Database_Editors
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BindData();
+        }
+
+        public void BindData()
+        {
+            MySqlConnection con = new MySqlConnection("Server=myServerAddress;Port=3306;Database=myDataBase;Uid=myUsername;Pwd = myPassword; "); //needs a fix for user to add in the data.
+            con.Open();
+            
+
+            MySqlCommand cmd = new MySqlCommand("select + from person", con);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+
+            //Needs a new fix!
+            //GridView1.DataSource = ds;
+            //GridView1.DataBind(); 
+            cmd.Dispose();
+            con.Close();
         }
     }
 }
