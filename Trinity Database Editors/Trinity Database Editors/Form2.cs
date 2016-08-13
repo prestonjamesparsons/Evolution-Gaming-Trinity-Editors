0717b9e7c8086088d961b1ec178ee343e89949ee
dan <danilo.naid@gmail.com>
2016-08-14 00:54:43 +0200

    registration saved in db.

--- Trinity Database Editors/Trinity Database Editors/Form2.cs
+++ Trinity Database Editors/Trinity Database Editors/Form2.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 
 namespace Trinity_Database_Editors
@@ -19,7 +20,31 @@ namespace Trinity_Database_Editors
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //no username?
+            string myConnection = "datasource=YOURSOURCE;port=3306;username=YOURUSERNAME;password=YOURPASSWORD";
+            MySqlConnection myConn = new MySqlConnection(myConnection);
+            MySqlCommand SelectCommand = new MySqlCommand("select * from dbname.accounts where firstname='" + this.textBox1.Text +  "' surname='" + this.textBox2.Text + "' email='" + this.textBox3.Text + "' password= '" + this.textBox4.Text + "'and terms= '" + this.checkBox1 + "';'", myConn);
+            MySqlDataReader myReader;
+            myConn.Open();
+            myReader = SelectCommand.ExecuteReader();
+            int count = 0;
+            while (myReader.Read())
+            {
+                count = count + 1;
+            }
+            if (count == 1)
+            {
+                MessageBox.Show("Username and password is correct");
+            }
+            else if (count > 1)
+            {
+                MessageBox.Show("Access denied");
+            }
+            else
+            {
+                MessageBox.Show("Wrong username and password");
+            }
+            myConn.Close();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
